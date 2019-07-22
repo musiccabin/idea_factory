1198f5dada734e9de2409bdd7dc1a61af5258ad1
musiccabin <thisis.susannahsun@gmail.com>
2019-07-22 14:23:06 -0700

    added tests

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -3,52 +3,91 @@ require 'rails_helper'
 RSpec.describe IdeasController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+    context 'no user signed in' do
+      it 'redirects to sign in page' do
+        get :new
+        expect(response).to redirect_to new_session_path
+      end
+
+      it 'has a flash alert' do
+        get :new
+        expect(flash[:alert]).to be
+      end
+    end
+
+    context 'user signed in' do
+      def sign_user_in
+        @user = FactoryBot.create(:user)
+        session[:user_id] = @user.id
+      end
+
+      it "creates a new instance of Idea" do
+        sign_user_in
+        get :new
+        expect(assigns(:idea)).to be_a_new Idea
+      end
+
+      it 'renders new view' do
+        sign_user_in
+        session[:user_id] = User.first.id
+        get :new
+        expect(response).to render_template :new
+      end
     end
   end
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    context 'user signed in' do
+      context 'validation succeeds' do
+        def valid_request
+          @user = FactoryBot.create(:user)
+          session[:user_id] = @user.id
+          post(:create, params: {idea: {title:'title',description:'description',user:@user}})
+        end
+
+        it 'saves the created Idea to db' do
+          count_before = Idea.count
+          valid_request
+          count_after = Idea.count
+          expect(count_before).to eq(count_after - 1)
+        end
+  
+        it 'redirects to idea show page' do
+          valid_request
+          expect(response).to redirect_to idea_path(Idea.last)
+        end
+      end
+
+      context 'validation fails' do
+        def invalid_request
+          @user = FactoryBot.create(:user)
+          session[:user_id] = @user.id
+          post(:create, params: {idea: {description:'description'}})
+        end
+        it 'renders new view' do
+          invalid_request
+          expect(response).to render_template :new
+        end
+
+        it 'did not save anything to db' do
+          count_before = Idea.count
+          invalid_request
+          count_after = Idea.count
+          expect(count_before).to eq(count_after)
+        end
+      end
+    end
+
+    context 'no user signed in' do
+      it 'redirects to sign in page' do
+        post(:create, params: {idea: {description:'description'}})
+        expect(response).to redirect_to new_session_path
+      end
+
+      it 'has a flash alert' do
+        post(:create, params: {idea: {description:'description'}})
+        expect(flash[:alert]).to be
+      end
     end
   end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #delete" do
-    it "returns http success" do
-      get :delete
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :idea do
-    title { "MyString" }
-    description { "MyText" }
-    user { nil }
+    title { Faker::Quotes }
+    description { Faker::Quotes::Shakespeare.romeo_and_juliet_quote }
+    association(:user, factory: :user)
   end
 end
